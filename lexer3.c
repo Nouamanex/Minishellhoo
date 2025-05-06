df19bb02c2c9ef59d3b2b59683df29fe22d59030
nouamanex <nouamanex@github.com>
2025-05-06 17:02:26 +0100

    pp

--- lexer3.c
+++ lexer3.c
@@ -3,68 +3,65 @@
 /*                                                        :::      ::::::::   */
 /*   lexer3.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nchagour <nchagour@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nouamane <nouamane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 15:29:42 by nchagour          #+#    #+#             */
-/*   Updated: 2025/05/05 21:09:04 by nchagour         ###   ########.fr       */
+/*   Updated: 2025/05/06 17:01:52 by nouamane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 //working...
+//mazal khdam 3liha
 
-void replace_env(t_token **tokliste)
-{
-    char *str;
-    int i;
-    int len_str;
-    char *var_env;
-    char *env_value;
-
-    while (*tokliste)
-    {
-        str = (*tokliste)->content;
-        i = 0;
-        while (str[i])
-        {
-            if (str[i] == '$' && str[i + 1])
-            {
-                i++;
-                len_str = 0;
-                len_str = ft_strlen(&str[i]);
-                var_env = ft_substr(str, i, len_str);
-                env_value = getenv(var_env);
-                free(var_env);
-                if (env_value)
-                {
-                    free((*tokliste)->content);
-                    (*tokliste)->content = ft_strdup(env_value);
-                }
-                break; // stop after first env replacement
-            }
-            i++;
-        }
-        tokliste = &(*tokliste)->next;
-    }
-}
-
-
-// void    env_get(t_token *tokliste)
+// void replace_env(t_token **tokliste)
 // {
-//     char *afterDollar;
-//     char *get_env;
-//     afterDollar = NULL;
-//     get_env = NULL;
-//     while (tokliste)
+//     char *str;
+//     int i;
+//     int j;
+//     int len_str;
+//     char *var_env;
+//     char *env_value;
+//     int befor;
+//     char *strbefor;
+//     char *total;
+
+//     befor = 0;
+//     j = 0;
+//     while (*tokliste)
 //     {
-//         if (tokliste->type == X_WORD && checkDollar(tokliste->content))
+//         str = (*tokliste)->content;
+//         i = 0;
+//         while (str[i])
 //         {
-//             afterDollar = checkDollar(tokliste->content);
-//             get_env = getenv(afterDollar);
-//             (*tokliste)->content = get_env;
+//             if (str[i] == '$' && str[i + 1])
+//             {
+//                 befor = i;
+//                 i++;
+//                 len_str = 0;
+//                 len_str = ft_strlen(&str[i]);
+//                 var_env = ft_substr(str, i, len_str);
+//                 env_value = getenv(var_env);
+//                 free(var_env);
+//                 if (befor > 0)
+//                 {
+//                     j = 0;
+//                     while (j < befor)
+//                     {
+//                         strbefor[j] = str[j];
+//                         j++;
+//                     }
+//                 }
+//                 if (env_value)
+//                 {
+//                     free((*tokliste)->content);
+//                     (*tokliste)->content = ft_strdup(env_value);
+//                 }
+//                 break;
+//             }
+//             i++;
 //         }
-//         (*tokliste) = (*tokliste)->next;
+//         tokliste = &(*tokliste)->next;
 //     }
-//     // return get_env;
 // }
